7ef3e35913b485db395c5c1a1084da99974b68b3
Dara Kilicoglu <darakilicoglu@gmail.com>
2018-08-05 22:34:00 +0200

    Add option to select scales

--- src/os.ino
+++ src/os.ino
@@ -12,9 +12,10 @@ AudioOutputI2S           audioOutput;
 AudioSynthWaveformDc     cvOut1;
 AudioConnection          patchCord2(cvOut1, 0, audioOutput, 0);
 
-int scales[1][5] = {
-  { 0, 3, 5, 7, 10 }  // pentatonicMinC[5]
-};
+AudioControlSGTL5000     sgtl5000_1;
+
+int upperPotInput = 20;
+int lowerPotInput = 21;
 
 float octave = 1.0;
 const float vPerSemitone = 0.08333;
@@ -22,9 +23,33 @@ const float vPerSemitone = 0.08333;
 int scale = 0, note = 0;
 bool busy = false;
 
+int scales[4][5] = {
+  { 0, 3, 5, 7, 10 },               // Minor C Pentatonic
+  { 0, 4, 7, 11, 12 },              // Paradiso
+  { 8, 14, 12, 17, 19 }             // Pygmy
+};
+
+String style[] = { "Up", "Down", "Up/Down", "Down/Up", "Random" };
+
 void setup() {
   Serial.begin(9600);
   AudioMemory(20);
+
+  // Enable the audio shield, select input, and enable output
+  sgtl5000_1.enable();
+  sgtl5000_1.inputSelect(AUDIO_INPUT_LINEIN);
+  sgtl5000_1.volume(0.82);
+  sgtl5000_1.adcHighPassFilterDisable();
+  sgtl5000_1.lineInLevel(0,0);
+  sgtl5000_1.unmuteHeadphone();
+}
+
+void listenPots() {
+  int upperPotValue = analogRead(upperPotInput); // scale selector
+  scale = map(upperPotValue, 0, 1023, 0, 2);
+
+  // int lowerPotValue = analogRead(lowerPotInput); // arpeggio style
+  delay(5);
 }
 
 void sync() {
@@ -43,6 +68,8 @@ void sync() {
 }
 
 void loop() {
+  listenPots();
+
   if (clkIn.available()) {
     sync();
   }
